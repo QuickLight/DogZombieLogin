caba0c26beb9ad3c2f8ccfda07500223038b3bdc
GluttonFox <16418304@qq.com>
2023-07-07 11:46:07 +0800

    1

---
+++ src/main/java/icu/dogcraft/dogzombielogin/DogZombieLogin.java
@@ -0,0 +1,17 @@
+package icu.dogcraft.dogzombielogin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DogZombieLogin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
